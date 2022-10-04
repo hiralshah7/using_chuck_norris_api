3335939826bddc8949b1d62068bb7ae143b2b17a
Hiral Shah <90278193+hiralshah7@users.noreply.github.com>
2022-10-04 13:20:37 -0400

    adding buttons

--- LearningNPM_Fall22-main/js/main.js
+++ LearningNPM_Fall22-main/js/main.js
@@ -8,7 +8,8 @@ import { getData } from "./modules/dataMiner.js";
     console.log('fired!');
 
     let theTeam = document.querySelector('#team-section'),
-        theTemplate = document.querySelector('#bio-template').content;
+        theTemplate = document.querySelector('#bio-template').content,
+        buttonContainer = document.querySelector('.queryControls') 
 
         // just a test to see if this is imported properly
 
@@ -42,6 +43,39 @@ import { getData } from "./modules/dataMiner.js";
             jokeText.textContent = joke.value;
             // debugger;
         }
-    getData('./data.json',buildTeam);
-    getData('https://api.chucknorris.io/jokes/random', buildJoke)
+
+        function addCatbuttons (categories) {
+            // debugger;
+            let activeButtons = categories.slice(0,6);
+
+            // below is the filter script to remove the explicit content or any other type of content use the below script 
+
+            // let activeButtons = categories.filter(item => item !== 'explicit').slice(0,6);
+
+            activeButtons.forEach(button => {
+                let buttonEl = `<button class="joke-button" data-cat=${button}>${button}</button>`;
+
+                buttonContainer.innerHTML += buttonEl;
+            })
+        }
+
+        function getJoke(event) {
+            // debugger;
+            // if there is not a category on a click then dont do anything
+            // because that will just throw an error => category targetCategory will not be defined
+            if (!event.target.dataset) { return;}
+            // if there is a custom data attribute clicked on a button , retrieve it and use that as the category
+            // query parameter
+
+            let targetCategory = event.target.dataset.cat;
+
+            getData(`https://api.chucknorris.io/jokes/random?category=${targetCategory}`, buildJoke);
+        }
+    // getData('./data.json',buildTeam);
+    // getData('https://api.chucknorris.io/jokes/random', buildJoke)
+
+    // use event delegation to handle click on each buttons
+    buttonContainer.addEventListener('click', getJoke);
+
+    getData ('https://api.chucknorris.io/jokes/categories', addCatbuttons);
 })();
